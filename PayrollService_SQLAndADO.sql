29567dc7bbf85c29af2fae43b5ba8b74b7c4a364
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-12 17:26:12 +0530

    [somu]Add:Creating Table Employee Roll in DataBase.

--- PayrollService_SQLAndADO.sql
+++ PayrollService_SQLAndADO.sql
@@ -1,3 +1,8 @@
 --Section-1(MSSQL-DB)
 --UC1
 Create DataBase PayRollService
+--UC2
+Create Table Employee_PayRoll(Id Int Primary key identity(1,1),
+Name varchar(20),Salary BigInt,StartDate Datetime)
+
+Select * from Employee_PayRoll
