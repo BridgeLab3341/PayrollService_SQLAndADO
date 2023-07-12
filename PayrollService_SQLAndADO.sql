6d4fe8f89375a7feec8776066d384c905f4ae120
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-12 17:43:09 +0530

    [somu]Add:Inserting Data Employee Payroll as Part of CRUD Operation.

--- PayrollService_SQLAndADO.sql
+++ PayrollService_SQLAndADO.sql
@@ -1,8 +1,17 @@
 --Section-1(MSSQL-DB)
 --UC1
 Create DataBase PayRollService
+
 --UC2
 Create Table Employee_PayRoll(Id Int Primary key identity(1,1),
 Name varchar(20),Salary BigInt,StartDate Datetime)
 
-Select * from Employee_PayRoll
+--UC3
+Insert Into Employee_PayRoll(Name,Salary,StartDate) values('Somu',120000,2023-08-28)
+Insert Into Employee_PayRoll(Name,Salary,StartDate) values('Anil',140000,2022-08-22)
+Insert Into Employee_PayRoll(Name,Salary,StartDate) values('Sandy',100000,2020-04-23)
+Insert Into Employee_PayRoll(Name,Salary,StartDate) values('Ganesh',150000,2021-02-14)
+Insert Into Employee_PayRoll(Name,Salary,StartDate) values('Mani',160000,2023-04-03)
+Insert Into Employee_PayRoll(Name,Salary,StartDate) values('Rani',110000,2019-03-03)
+Insert Into Employee_PayRoll(Name,Salary,StartDate) values('Shekar',100000,2021-08-19)
+Insert Into Employee_PayRoll(Name,Salary,StartDate) values('Kumar',150000,2020-06-08)
